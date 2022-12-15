892b130ff71dd69c11a42389328d6756f97a12c0
Suhail Ahamed <ahamed@semvox.de>
2022-12-15 15:53:16 +0100

    Updated Formatcheck and annotation guidelines

--- app/formatcheck.py
+++ app/formatcheck.py
@@ -30,35 +30,39 @@ def check_decomposition(question,text,nlp):
     for word in text.split(): 
         if word.lower().strip("\"").strip(".") in entities: # check for entity occurences
             entities.remove(word.lower().strip("\"").strip("."))
-    lines = text.split("\n") # get each decomposition
-    reference_pattern = "#([0-9]+)?"
-    n = len(lines) # get amount of decompositions
-    if lines[0].startswith("return"): # check if the first statement starts with return
-        if "return" in lines[0][5:]:
-            return DecompositionError("Each decomposition must start in a new line")
-        if len(lines) > 1: # if there is more than one statement
-            needed_references = {1} # set of statements that need to be referenced (index 0 = statement 1)
-        else:
-            needed_references = set() # don't need reference when there is only one statement
-        for i in range(1,n): # check for all further lines
-            if "return" in lines[i][6:]:
-                return ReturnError("Each decomposition must start in a new line")
-            if "#" in lines[i]: # find cross-references
-                matches = re.findall(reference_pattern,lines[i]) # find all references
-                if any([int(x)>=i+1 for x in matches]): # if a reference is higher than the current decomposition
-                    return DecompositionError("Statement " + str(i+1) + " contains a reference that is not (yet) defined")
-                else:
-                    for x in matches: # matches that are syntactically correct in this statement
-                        if int(x) in needed_references: # if it has not been crossed out previously, i.e. due to a previous reference to the same statement
-                            needed_references.remove(int(x)) # for overview i´that all statements are referenced at least once
-            if i+1 != n: # if the current line is not the last one
-                needed_references.add(i+1) # current statement will also need to be referenced at some point
-            if not lines[i].startswith(";return"):
-                return ReturnError("Statement " + str(i+1) + " must start with ;return")
-        if needed_references != set():
-            return DecompositionError("Undefined References! " + str(needed_references))
+
+    if "\n" in text:
+        return ReturnError("Annotations must be in a single line")
     else:
-        return ReturnError("First statement must start with return")
+        lines = text.split(";") # get each decomposition
+        reference_pattern = "#([0-9]+)?"
+        n = len(lines) # get amount of decompositions
+        if lines[0].startswith("return"): # check if the first statement starts with return
+            if "return" in lines[0][5:]:
+                return DecompositionError("Each decomposition must start after a semicolon")
+            if len(lines) > 1: # if there is more than one statement
+                needed_references = {1} # set of statements that need to be referenced (index 0 = statement 1)
+            else:
+                needed_references = set() # don't need reference when there is only one statement
+            for i in range(1,n): # check for all further lines
+                if "return" in lines[i][6:]:
+                    return ReturnError("Each decomposition must start after a semicolon")
+                if "#" in lines[i]: # find cross-references
+                    matches = re.findall(reference_pattern,lines[i]) # find all references
+                    if any([int(x)>=i+1 for x in matches]): # if a reference is higher than the current decomposition
+                        return DecompositionError("Statement " + str(i+1) + " contains a reference that is not (yet) defined")
+                    else:
+                        for x in matches: # matches that are syntactically correct in this statement
+                            if int(x) in needed_references: # if it has not been crossed out previously, i.e. due to a previous reference to the same statement
+                                needed_references.remove(int(x)) # for overview i´that all statements are referenced at least once
+                if i+1 != n: # if the current line is not the last one
+                    needed_references.add(i+1) # current statement will also need to be referenced at some point
+                if not lines[i].startswith("return"):
+                    return ReturnError("Statement " + str(i+1) + " must start with return")
+            if needed_references != set():
+                return DecompositionError("Undefined References! " + str(needed_references))
+        else:
+            return ReturnError("First statement must start with return")
     if entities != set():
         return EntityWarning("The following entities occur in the question but not in the decompositions: " + str(entities))
     return {'type': "OK", 'message': ""}
